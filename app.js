708647888fcb8a7fc33e99a16ac471374c93f64c
Gadiel-S <gadielbarrosdesouza@gmail.com>
2024-07-18 16:31:45 -0300

    commit teste

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.get('/', (req, res) => {
 });
 
 // Exemplo de rota POST
+app.post('/', (req, res) => {
+  res.send('Testando!');
+});
+
+
+// Exemplo de rota POST
 app.post('/api/exemplo', (req, res) => {
   res.send('Rota POST acessada');
 });

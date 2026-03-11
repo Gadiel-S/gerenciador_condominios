77cee0380f215e60e7a42e45c4d7a936e5df1877
Gadiel-S <gadielbarrosdesouza@gmail.com>
2026-03-10 22:36:30 -0300

    Criando a base do projeto refatorado e configurações iniciais

---
+++ api-java/src/main/java/br/com/gadiel/Startup.java
@@ -0,0 +1,13 @@
+package br.com.gadiel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJavaApplication.class, args);
+	}
+
+}
